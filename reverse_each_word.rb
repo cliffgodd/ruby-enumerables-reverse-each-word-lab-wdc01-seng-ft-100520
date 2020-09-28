6bc3a0977433cedbcb1b5f7b9a4cd8a50be580b4
Justin Clifford <justoon7.jc@gmail.com>
2020-09-28 21:54:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ def reverse_each_word(string)
     key.reverse
     #collect the reverse order of each key in the string array into the the new array
   end
-  new_array.join(" ")
-  
+  pp new_array.join(" ")
+
   #after collecting the new array i am joining the new array to converrt it back to a string and then it returns the new string as well
 end
